68c6f755b045763c49c765c016ff473f4c2067d3
jauhhari <ervanadiwijaya@gmail.com>
2021-06-30 15:31:01 +0700

    OK

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var numb = 20;
+var name = "franggi";
+var isempty = false;
+var denied = null;
+var notknown;
+
+console.log(numb);
+console.log(name);
+console.log(isempty);
+console.log(denied);
+console.log(notknown);
